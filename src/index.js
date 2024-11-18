9d99bb0dbbe13db3c2986b2c4e154b1735565639
priyanshu singh <priyanshusingh@priyanshus-MacBook-Air.local>
2024-11-18 17:33:27 +0530

    completed basic sequelize setup for creating a db

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {PORT}=require('./config/serverConfig')
 const setupAndStartServer=async()=>{
     //create the express object
     const app=express();
+    //middlewares 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
