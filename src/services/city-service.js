1e36dc1a3f30e8d3e9c9784f11045fd61e146bc1
priyanshu singh <priyanshusingh@priyanshus-MacBook-Air.local>
2024-11-22 11:06:19 +0530

    Added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,46 @@
+const {CityRepository} =require('../repository/index');
+
+class CityService {
+    constructor(){
+        this.cityRepository=new  CityRepository();
+
+    }
+    async createCity(data){
+       try {
+           const city=await this.cityRepository.createCity(data);
+           return city;
+       } catch (error) {
+          console.log("Something went wrong at service layer");
+          throw{error};
+       }
+    }
+    async deleteCity(cityID){
+        try {
+           const response=this.cityRepository.deleteCity(cityID);
+           return response;
+        } catch (error) {
+           console.log("Something went wrong at service layer");
+           throw{error};
+        }
+    }
+    async updateCity(cityID,data){
+        try {
+            const city=await this.cityRepository.updateCity(cityID,data);
+            return city;
+        } catch (error) {
+           console.log("Something went wrong at service layer");
+           throw{error};
+        }
+    }
+    async getCity(cityID){
+        try {
+           const city=await this.cityRepository.getCity(cityID);
+           return city;
+        } catch (error) {
+           console.log("Something went wrong at service layer");
+           throw{error};
+        }
+    }
+
+}
+module.exports=CityService;
