ed85c3fb92c5c89161282256cd29381d46b67478
priyanshu singh <priyanshusingh@priyanshus-MacBook-Air.local>
2024-11-22 10:46:10 +0530

    Completed city Repository

--- src/repository/city_repository.js
+++ src/repository/city_repository.js
@@ -6,6 +6,7 @@ class CityRepository{
             const city=await City.create({name});
             return city;
         }catch(error){
+          console.log("Something went wrong in the repository layer");
           throw{error};
         }
     }
@@ -17,9 +18,35 @@ class CityRepository{
                     id:CityID
                 }
             });
+            return true;
            
         }catch(error){
+           console.log("Something went wrong in the repository layer");
            throw{error};
+        }
+    }
+
+    async updateCity(cityID,data){
+        try {
+            const city=await City.update(data,{
+                where:{
+                    id:cityID,
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw{error};
+        }
+    }
+
+    async getCity(cityID){
+        try{
+           const city=await City.findByPK(cityID);
+           return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw{error};
         }
     }
 }
