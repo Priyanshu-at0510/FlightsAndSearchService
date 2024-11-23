c7e26c14f5013f4f15eca1496334866d4ad9d559
priyanshu singh <priyanshusingh@priyanshus-MacBook-Air.local>
2024-11-23 22:41:29 +0530

    Added Routers to complete create api

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const express= require("express");
 const bodyParser= require('body-parser');
 
 const {PORT}=require('./config/serverConfig');
-
-
+const ApiRoutes=require('./routes/index');
+ 
 const setupAndStartServer=async()=>{
     //create the express object
     const app=express();
     //middlewares 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',ApiRoutes);
     app.listen(PORT, ()=>{
        console.log(`server started at ${PORT}`);
      
